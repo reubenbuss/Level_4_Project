720fcb671f05a54bea1dccac0f9bf5b270638b84
ReubenLaptop <reubenbuss@hotmail.com>
2023-01-25 09:57:42 +0000

    Changes to be committed: 	modified:   Coffee_Data/Coffee_Data_Plot.py 	new file:   Coffee_Data/Ensemble_Of_Models_On_Coffee_Data.py 	renamed:    Coffee_Data/LightGBM_On_ -> Coffee_Data/LightGBM_On_Coffee_Data.py 	new file:   Statistical_Method/Testing_Linear_Regression_Assumptions.py

--- Coffee_Data/Coffee_Data_Plot.py
+++ Coffee_Data/Coffee_Data_Plot.py
@@ -4,7 +4,6 @@ from matplotlib import pyplot as plt
 df = pd.read_csv(r"C:\Users\reube\OneDrive - Durham University\Documents\Year 4\Project\Data\Coffee\Categorised_Coffee_Data.csv")
 df = df.sample(frac=1)
 fig = plt.figure(dpi=2400)
-
 for i in range(0,df.shape[0]):
     if df.iloc[i,0] == "green_control":
         COLOUR = "Green"
@@ -15,12 +14,13 @@ for i in range(0,df.shape[0]):
     if df.iloc[i,0] == "AribicavarGeisha":
         COLOUR = "blue"
     y=df.values[i,1:]
-    plt.scatter(list(df)[1:],y,s=0.01,c=COLOUR)
-plt.scatter(0,-20,color = "Green",label = "Green_Control")
-plt.scatter(0,-20,color = "brown",label = "Rust")
+    plt.scatter(df.columns[1:],y,s=0.01,c=COLOUR,marker="o")
+plt.scatter(0,-50,color = "Green",label = "Green_Control")
+plt.scatter(1655,-50,color = "brown",label = "Rust")
 plt.scatter(0,-20,color = "red",label = "Rust_Canopy")
 plt.scatter(0,-20,color = "blue",label = "AribicavarGeisha")
-plt.xticks(df.columns[1::200],labels=range(180,1021,84))
+plt.vlines(x=[280,1655], ymin=0, ymax=95, colors='black', ls=':', lw=2, label='Acceptable Range')
+plt.xticks(list(range(0,2001,400)),[187,370,544,710,865,1010])
 plt.ylim(-10,100)
 plt.legend(loc="upper left")
 plt.title("Coffee Data")
---
+++ Statistical_Method/Testing_Linear_Regression_Assumptions.py
@@ -0,0 +1,229 @@
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
+from statsmodels.stats.diagnostic import normal_ad
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.stats.stattools import durbin_watson
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv(r"C:\Users\reube\OneDrive - Durham University\Documents\Year 4\Project\Data\RedWhiteBlack Non Erroneous Data.csv")
+label = (df.Species).to_numpy()
+features = df.drop(["Species"],axis=1,inplace=False)
+feature_names = features.columns
+for i in range(0,label.shape[0]):
+    if label[i] == "Black":
+        label[i] = 0.1
+    if label[i] == "Red":
+        label[i] = 0.5
+    if label[i] == "White":
+        label[i] = 0.9
+
+
+def linear_regression_assumptions(features, label, feature_names=None):
+    """
+    Tests a linear regression on the model to see if assumptions are being met
+    """
+    
+    # Setting feature names to x1, x2, x3, etc. if they are not defined
+    if feature_names is None:
+        feature_names = ['X'+str(feature+1) for feature in range(features.shape[1])]
+    
+    print('Fitting linear regression')
+    # Multi-threading if the dataset is a size where doing so is beneficial
+    if features.shape[0] < 100000:
+        model = LinearRegression(n_jobs=-1)
+    else:
+        model = LinearRegression()
+        
+    model.fit(features, label)
+    
+    # Returning linear regression R^2 and coefficients before performing diagnostics
+    r2 = model.score(features, label)
+    print()
+    print('R^2:', r2, '\n')
+    print('Coefficients')
+    print('-------------------------------------')
+    print('Intercept:', model.intercept_)
+    
+    for feature in range(len(model.coef_)):
+        print('{0}: {1}'.format(feature_names[feature], model.coef_[feature]))
+
+    print('\nPerforming linear regression assumption testing')
+    
+    # Creating predictions and calculating residuals for assumption tests
+    predictions = model.predict(features)
+    df_results = pd.DataFrame({'Actual': label, 'Predicted': predictions})
+    df_results['Residuals'] = abs(df_results['Actual']) - abs(df_results['Predicted'])
+
+    
+    def linear_assumption():
+        """
+        Linearity: Assumes there is a linear relationship between the predictors and
+                   the response variable. If not, either a polynomial term or another
+                   algorithm should be used.
+        """
+        print('\n=======================================================================================')
+        print('Assumption 1: Linear Relationship between the Target and the Features')
+        
+        print('Checking with a scatter plot of actual vs. predicted. Predictions should follow the diagonal line.')
+        
+        # Plotting the actual vs predicted values
+        sns.lmplot(x='Actual', y='Predicted', data=df_results, fit_reg=False, height=7)
+        
+        # Plotting the diagonal line
+        line_coords = np.arange(df_results.min().min(), df_results.max().max())
+        plt.plot(line_coords, line_coords,  # X and y points
+                 color='darkorange', linestyle='--')
+        plt.title('Actual vs. Predicted')
+        plt.show()
+        print('If non-linearity is apparent, consider adding a polynomial term')
+        
+        
+    def normal_errors_assumption(p_value_thresh=0.05):
+        """
+        Normality: Assumes that the error terms are normally distributed. If they are not,
+        nonlinear transformations of variables may solve this.
+               
+        This assumption being violated primarily causes issues with the confidence intervals
+        """
+        print('\n=======================================================================================')
+        print('Assumption 2: The error terms are normally distributed')
+        print()
+    
+        print('Using the Anderson-Darling test for normal distribution')
+
+        # Performing the test on the residuals
+        p_value = normal_ad(df_results['Residuals'])[1]
+        print('p-value from the test - below 0.05 generally means non-normal:', p_value)
+    
+        # Reporting the normality of the residuals
+        if p_value < p_value_thresh:
+            print('Residuals are not normally distributed')
+        else:
+            print('Residuals are normally distributed')
+    
+        # Plotting the residuals distribution
+        plt.subplots(figsize=(12, 6))
+        plt.title('Distribution of Residuals')
+        sns.distplot(df_results['Residuals'])
+        plt.show()
+    
+        print()
+        if p_value > p_value_thresh:
+            print('Assumption satisfied')
+        else:
+            print('Assumption not satisfied')
+            print()
+            print('Confidence intervals will likely be affected')
+            print('Try performing nonlinear transformations on variables')
+        
+        
+    def multicollinearity_assumption():
+        """
+        Multicollinearity: Assumes that predictors are not correlated with each other. If there is
+                           correlation among the predictors, then either remove prepdictors with high
+                           Variance Inflation Factor (VIF) values or perform dimensionality reduction
+                           
+                           This assumption being violated causes issues with interpretability of the 
+                           coefficients and the standard errors of the coefficients.
+        """
+        print('\n=======================================================================================')
+        print('Assumption 3: Little to no multicollinearity among predictors')
+        
+        # Plotting the heatmap
+        plt.figure(figsize = (10,8))
+        sns.heatmap(pd.DataFrame(features, columns=feature_names).corr(), annot=True)
+        plt.title('Correlation of Variables')
+        plt.show()
+        
+        print('Variance Inflation Factors (VIF)')
+        print('> 10: An indication that multicollinearity may be present')
+        print('> 100: Certain multicollinearity among the variables')
+        print('-------------------------------------')
+       
+        # Gathering the VIF for each variable
+        VIF = [variance_inflation_factor(features, i) for i in range(features.shape[1])]
+        for idx, vif in enumerate(VIF):
+            print('{0}: {1}'.format(feature_names[idx], vif))
+        
+        # Gathering and printing total cases of possible or definite multicollinearity
+        possible_multicollinearity = sum([1 for vif in VIF if vif > 10])
+        definite_multicollinearity = sum([1 for vif in VIF if vif > 100])
+        print()
+        print('{0} cases of possible multicollinearity'.format(possible_multicollinearity))
+        print('{0} cases of definite multicollinearity'.format(definite_multicollinearity))
+        print()
+
+        if definite_multicollinearity == 0:
+            if possible_multicollinearity == 0:
+                print('Assumption satisfied')
+            else:
+                print('Assumption possibly satisfied')
+                print()
+                print('Coefficient interpretability may be problematic')
+                print('Consider removing variables with a high Variance Inflation Factor (VIF)')
+        else:
+            print('Assumption not satisfied')
+            print()
+            print('Coefficient interpretability will be problematic')
+            print('Consider removing variables with a high Variance Inflation Factor (VIF)')
+        
+        
+    def autocorrelation_assumption():
+        """
+        Autocorrelation: Assumes that there is no autocorrelation in the residuals. If there is
+                         autocorrelation, then there is a pattern that is not explained due to
+                         the current value being dependent on the previous value.
+                         This may be resolved by adding a lag variable of either the dependent
+                         variable or some of the predictors.
+        """
+        print('\n=======================================================================================')
+        print('Assumption 4: No Autocorrelation')
+        print('\nPerforming Durbin-Watson Test')
+        print('Values of 1.5 < d < 2.5 generally show that there is no autocorrelation in the data')
+        print('0 to 2< is positive autocorrelation')
+        print('>2 to 4 is negative autocorrelation')
+        print('-------------------------------------')
+        durbinWatson = durbin_watson(df_results['Residuals'])
+        print('Durbin-Watson:', durbinWatson)
+        if durbinWatson < 1.5:
+            print('Signs of positive autocorrelation', '\n')
+            print('Assumption not satisfied', '\n')
+            print('Consider adding lag variables')
+        elif durbinWatson > 2.5:
+            print('Signs of negative autocorrelation', '\n')
+            print('Assumption not satisfied', '\n')
+            print('Consider adding lag variables')
+        else:
+            print('Little to no autocorrelation', '\n')
+            print('Assumption satisfied')
+
+            
+    def homoscedasticity_assumption():
+        """
+        Homoscedasticity: Assumes that the errors exhibit constant variance
+        """
+        print('\n=======================================================================================')
+        print('Assumption 5: Homoscedasticity of Error Terms')
+        print('Residuals should have relative constant variance')
+        
+        # Plotting the residuals
+        plt.subplots(figsize=(12, 6))
+        ax = plt.subplot(111)  # To remove spines
+        plt.scatter(x=df_results.index, y=df_results.Residuals, alpha=0.5)
+        plt.plot(np.repeat(0, df_results.index.max()), color='darkorange', linestyle='--')
+        ax.spines['right'].set_visible(False)  # Removing the right spine
+        ax.spines['top'].set_visible(False)  # Removing the top spine
+        plt.title('Residuals')
+        plt.show() 
+        print('If heteroscedasticity is apparent, confidence intervals and predictions will be affected')
+        
+        
+    linear_assumption()
+    normal_errors_assumption()
+    multicollinearity_assumption()
+    autocorrelation_assumption()
+    homoscedasticity_assumption()
+
+linear_regression_assumptions(features, label, feature_names)
